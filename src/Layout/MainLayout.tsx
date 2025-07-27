d02ffd3888d4c9c0cb613601cc2bcef0b4e5cf1e
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-07-27 19:47:26 +0300

    27/7/2025

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -7,11 +7,11 @@ import type { RootState } from '../redux/store';
 export default function MainLayout() {
   const isDark = useSelector((state: RootState) => state.dark.value);
   const open = useSelector((state: RootState) => state.open.value);
-
+const login = useSelector((state: RootState) => state.login.value);
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <div className={`flex-1 p-7  ${isDark?"bg-[#111827] text-white":"bg-white text-blue-500 "} ${open?"ml-70":"ml-20" } duration-300`}>
+      <div className={`flex-1 p-7  ${isDark?"bg-[#111827] text-white":"bg-white text-blue-500 "} ${login?open?"ml-70":"ml-20":"" } duration-300`}>
         <Outlet />
       </div>
     </div>
